b82f32f3ccf43e698c03894b0f3b69b86304a66f
Jonaslindner24 <lindne240455@sr.htlweiz.at>
2025-12-31 13:12:50 +0100

    Add files via upload

---
+++ Website/java.js
@@ -0,0 +1,25 @@
+function doPost(e) {
+    try {
+
+        // datei von website
+        var fileData = e.paramenter.datei;
+        var blob = e.paramenter.filecount;
+
+        // datei in google drive speichern
+        var folder = DriveApp.getRootFolder();
+        var contenttype = e.paramenter.mimeType;
+        var decoded = Utilities.base64Decode(e.parameter.fileData);
+        var file = folder.createFile(Utilities.newBlob(decoded, contenttype, e.paramenter.fileName));
+
+        // Email versenden
+        MailApp.sendEmail({
+            to: "Floopschnee@gmail.com",
+            subject: "Auftrag!",
+            body: "Name: " + e.paramenter.name + "\nE-Mail: " + e.paramenter.email + "\nDatei-Link: " + file.getUrl()
+        });
+
+        return ContentService.createTextOutput("Erfolg!").setMimeType(ContentService.Mimetype.TEXT);
+    }   catch (f) {
+        return ContentService.createTextOutput("Fehler: " + f.toString()).setMimeType(ContentService.MimeType.TEXT);
+  }
+}
